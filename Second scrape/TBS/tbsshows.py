dae7974de8dba0a1626e05bbb321ade39c1b2426
lisiq <lisqarkaxhija@gmail.com>
2021-04-14 09:10:41 +0200

    updated tbsshows.py

--- Second scrape/TBS/tbsshows.py
+++ Second scrape/TBS/tbsshows.py
@@ -75,48 +75,48 @@ for i in lines:
 
 # we define this helper function to get only the data 
 # that we need form the dictionaries
-# def get_info(n):
-#     m = n['potentialAction'][0]
-#     url = "\\/" + "/".join(n['@id'].split("/", 4)[3:]) 
-#     genre = m['@type']
-#     language = m['target']['inLanguage']
-#     bot_country = m['actionAccessibilityRequirement']['eligibleRegion']['name']
-#     availabilityStarts = m['actionAccessibilityRequirement']['availabilityStarts']
-#     availabilityEnds = m['actionAccessibilityRequirement']['availabilityEnds']
-#     bot_system = m['actionAccessibilityRequirement']['requiresSubscription']['name']
-#     offer_type = m['actionAccessibilityRequirement']['requiresSubscription']['authenticator']['name']
-#     return [url, genre, language, bot_country, availabilityStarts, availabilityEnds, bot_system, offer_type]
+def get_info(n):
+    m = n['potentialAction'][0]
+    url = "\\/" + "/".join(n['@id'].split("/", 4)[3:]) 
+    genre = m['@type']
+    language = m['target']['inLanguage']
+    bot_country = m['actionAccessibilityRequirement']['eligibleRegion']['name']
+    availabilityStarts = m['actionAccessibilityRequirement']['availabilityStarts']
+    availabilityEnds = m['actionAccessibilityRequirement']['availabilityEnds']
+    bot_system = m['actionAccessibilityRequirement']['requiresSubscription']['name']
+    offer_type = m['actionAccessibilityRequirement']['requiresSubscription']['authenticator']['name']
+    return [url, genre, language, bot_country, availabilityStarts, availabilityEnds, bot_system, offer_type]
 
 
 # we apply the helper function to all the dictionaries 
 # and ignore the files that do not have the data that we need
-# flat_list = []
-# for i in dict_list:
-#     try:
-#         flat_list.append(get_info(i))
-#     except KeyError:
-#         pass
+flat_list = []
+for i in dict_list:
+    try:
+        flat_list.append(get_info(i))
+    except KeyError:
+        pass
 
 # # here we create a data fram from the list overhead and name the columns
-# tbsshows_ldjson = pd.DataFrame(flat_list, 
-#                                columns=['url', 'genre', 'language', 'bot_country', 'availabilityStarts', 
-#                                         'availabilityEnds', 'bot_system', 'offer_type'])
+tbsshows_ldjson = pd.DataFrame(flat_list, 
+                               columns=['url', 'genre', 'language', 'bot_country', 'availabilityStarts', 
+                                        'availabilityEnds', 'bot_system', 'offer_type'])
 
 # finally we save the data as a csv file
-# tbsshows_ldjson.to_csv('/home/lisi/MediaBiz/Second scrape/TBS/tbsshows_ldjson.csv', index=False)
+tbsshows_ldjson.to_csv('/home/lisi/MediaBiz/Second scrape/TBS/tbsshows_ldjson.csv', index=False)
 
 # there is another json file that contain some more information
 # we will scrape also that
 # to scrape this we do not need all the link, we just need a link per season
 # since it containas information for every other episode
-# new_lines = []
-# for i in lines:
-#     match = "/".join(i.split("/", 4)[:4]) #split until fourth occurence of /, and then get first 4 elements, after that join
-#     if match not in new_lines:
-#         new_lines.append(match)
-#         new_lines.append(i)
+new_lines = []
+for i in lines:
+    match = "/".join(i.split("/", 4)[:4]) #split until fourth occurence of /, and then get first 4 elements, after that join
+    if match not in new_lines:
+        new_lines.append(match)
+        new_lines.append(i)
 
-# new_lines = new_lines[1::2]
+new_lines = new_lines[1::2]
 
 # we scrape all the json files and save them 
 for i in lines:
@@ -132,28 +132,28 @@ for i in lines:
 
 # we open the files transform them into dictionaries, make the syntax python friendly
 # and ignore the files that do not have the data that we need
-# flat_list = []
-# dict_list = []
-# for i in new_lines:
-#     try:
-#         with open('/home/lisi/MediaBiz/Second scrape/TBS/TBSshows_drupla-settings-json/' + i.encode("utf-8").hex() + '.json', 'r') as j:
-#             dict_list.append(dict(eval(json.load(j).replace('null', '"NaN"')\
-#                                        .replace('true',"True")\
-#                                        .replace('false',"False")))['turner_playlist'])
-#     except KeyError:
-#         pass
+flat_list = []
+dict_list = []
+for i in new_lines:
+    try:
+        with open('/home/lisi/MediaBiz/Second scrape/TBS/TBSshows_drupla-settings-json/' + i.encode("utf-8").hex() + '.json', 'r') as j:
+            dict_list.append(dict(eval(json.load(j).replace('null', '"NaN"')\
+                                       .replace('true',"True")\
+                                       .replace('false',"False")))['turner_playlist'])
+    except KeyError:
+        pass
 
 # # transform from a nested list into a list      
-# flat_list = [item for sublist in dict_list for item in sublist]
+flat_list = [item for sublist in dict_list for item in sublist]
 
 # # create a dataframe
-# tbsshows_drupla_settings_json = pd.DataFrame(flat_list)
+tbsshows_drupla_settings_json = pd.DataFrame(flat_list)
 
 # # finally save it
-# tbsshows_drupla_settings_json.to_csv('/home/lisi/MediaBiz/Second scrape/TBS/tbsshows_drupla_settings_json.csv', index=False)
+tbsshows_drupla_settings_json.to_csv('/home/lisi/MediaBiz/Second scrape/TBS/tbsshows_drupla_settings_json.csv', index=False)
 
 # # we combine the two datasets in one
-# df_tbsshows = pd.merge(tbsshows_drupla_settings_json, tbsshows_ldjson, how='left', on='url')
+df_tbsshows = pd.merge(tbsshows_drupla_settings_json, tbsshows_ldjson, how='left', on='url')
 
 # # save the merger
-# df_tbsshows.to_csv('/home/lisi/MediaBiz/Second scrape/TBS/tbsshows.csv', index=False)
+df_tbsshows.to_csv('/home/lisi/MediaBiz/Second scrape/TBS/tbsshows.csv', index=False)
